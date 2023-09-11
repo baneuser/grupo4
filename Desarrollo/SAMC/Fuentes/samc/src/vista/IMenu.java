1e2371a36d9217c2d5625e6ed6458d828ed09140
Gregory <softgregory20@gmail.com>
2023-09-11 15:33:44 -0500

    Proyectos ya desarrollados por la empresa Pluton SAC

---
+++ Desarrollo/SAMC/Fuentes/samc/src/vista/IMenu.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface IMenu extends IPadre {
+    
+    static final String TITULO_RAIZ = "BIENVENIDO ";
+}
